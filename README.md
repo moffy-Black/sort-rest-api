f43ebab2369730d770e709ebba12cfc3188266e8
moffy-Black <black.river.emperor@outlook.jp>
2022-12-05 16:26:05 +0900

    [作成] サーバ機能

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ go で作られた受け取った配列を heap sort して返すサーバ
 ```
 github.com/moffy-Black/sort-server
 ```
+
+## server sample test snippet
+
+```
+curl http://localhost:8080/ \                                               ✔  16:19:42 
+--header "Content-Type: application/json" \
+--include \
+--request "POST" \
+--data '{"sortType": "quicksort", "array": [0,1,9,2,8,3,7,4,6,5]}'
+```
--- main.go
+++ main.go
@@ -2,29 +2,57 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/moffy-Black/sort-rest-API/pkg/bubblesort"
 	"github.com/moffy-Black/sort-rest-API/pkg/heapsort"
 	"github.com/moffy-Black/sort-rest-API/pkg/quicksort"
+
+	"github.com/gin-gonic/gin"
 )
 
+type arr struct {
+	SortType string `json:"sortType"`
+	Array    []int  `json:"array"`
+}
+
+func responseSortArr(c *gin.Context) {
+	var recvArr arr
+
+	if err := c.BindJSON(&recvArr); err != nil {
+		return
+	}
+
+	switch recvArr.SortType {
+	case "bubblesort":
+		bubbleArr, err := bubblesort.BubbleSort(recvArr.Array)
+		if err != nil {
+			fmt.Println(err)
+			c.IndentedJSON(http.StatusBadRequest, err)
+		}
+		fmt.Println(`BubbleSort(sampleArr) = `, bubbleArr)
+		c.IndentedJSON(http.StatusOK, bubbleArr)
+	case "heapsort":
+		heapArr, err := heapsort.HeapSort(recvArr.Array)
+		if err != nil {
+			fmt.Println(err)
+			c.IndentedJSON(http.StatusBadRequest, err)
+		}
+		fmt.Println(`HeapSort(sampleArr) = `, heapArr)
+		c.IndentedJSON(http.StatusOK, heapArr)
+	case "quicksort":
+		quickArr, err := quicksort.QuickSort(recvArr.Array)
+		if err != nil {
+			fmt.Println(err)
+			c.IndentedJSON(http.StatusBadRequest, err)
+		}
+		fmt.Println(`QuickSort(sampleArr) = `, quickArr)
+		c.IndentedJSON(http.StatusOK, quickArr)
+	}
+}
+
 func main() {
-	sampleArr := []int{1, 9, 2, 8, 3, 7, 4, 6, 5, 0}
-	bubbleArr, err := bubblesort.BubbleSort(sampleArr)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(`BubbleSort(sampleArr) = `, bubbleArr)
-
-	heapArr, err := heapsort.HeapSort(sampleArr)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(`HeapSort(sampleArr) = `, heapArr)
-
-	quickArr, err := quicksort.QuickSort(sampleArr)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(`QuickSort(sampleArr) = `, quickArr)
+	router := gin.Default()
+	router.POST("/", responseSortArr)
+	router.Run("localhost:8080")
 }
